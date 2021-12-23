a535d857a032e1b16211e0f31b843f22dc19bf12
Talas111 <shirkopai@gmail.com>
2021-12-23 15:25:01 -0500

    second commit

---
+++ src/test/java/test/cincinnati/pages/login/LoginSecond.java
@@ -0,0 +1,8 @@
+package test.cincinnati.pages.login;
+
+public class LoginSecond {
+    public static void main(String[] args) {
+        System.out.println("My name is Shirin");
+        System.out.println("I need to learn how to code nd work with team in my command ");
+    }
+}
